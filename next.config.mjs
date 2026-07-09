872a6070812a9f3dcfc33afd47049b5893b06626
MariaSemenovaDev <semenovamk96@mail.ru>
2026-07-10 01:08:24 +0300

    Fix asset paths for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
-const repository = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-const isUserOrOrgPages = repository.endsWith(".github.io");
-const basePath =
-  process.env.GITHUB_ACTIONS && repository && !isUserOrOrgPages
-    ? `/${repository}`
-    : "";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   typedRoutes: true,
-  basePath,
-  assetPrefix: basePath || undefined
 };
 
 export default nextConfig;
